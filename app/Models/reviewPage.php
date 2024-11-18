1b41bd525ea725ab37ac8f7cb04fadb250679198
Falmnw <naufalwisaksono3112@gmail.com>
2024-11-18 22:34:54 +0700

    next

--- app/Models/reviewPage.php
+++ app/Models/reviewPage.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class reviewPage extends Model
+class ReviewPage extends Model
 {
     use HasFactory;
+
+    protected $table = 'review_pages';
+    protected $fillable = [
+        'judul',
+        'deskripsi',
+        'total_reviews',
+        'average_rating',
+    ];
 }
